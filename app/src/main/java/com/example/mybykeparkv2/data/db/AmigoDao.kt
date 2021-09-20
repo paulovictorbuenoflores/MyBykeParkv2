16f192314f44124d53f41090d1b834fb3a7a5c4c
Adriley <adrileysrb@gmail.com>
2021-09-20 15:38:17 -0300

    Add SqLite Dao classes

--- app/src/main/java/com/example/mybykeparkv2/data/db/AmigoDao.kt
+++ app/src/main/java/com/example/mybykeparkv2/data/db/AmigoDao.kt
@@ -1,7 +1,20 @@
 package com.example.mybykeparkv2.data.db
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.mybykeparkv2.data.model.Amigo
 
 @Dao
 interface AmigoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveAmigo(amigo : Amigo)
+
+    @Query("SELECT * FROM amigo")
+    suspend fun getAmigo():List<Amigo>
+
+    suspend fun editAmigo()
+
+    suspend fun deleteAmigo()
 }
