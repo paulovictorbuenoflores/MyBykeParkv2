ea8d68543e043ef1f838defbbd1e90af3ac847f8
Adriley <adrileysrb@gmail.com>
2021-09-20 15:32:18 -0300

    Bão sô Log Add

--- app/src/main/java/com/example/mybykeparkv2/MainActivity.kt
+++ app/src/main/java/com/example/mybykeparkv2/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.mybykeparkv2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("MYTAG", "Bao sô")
     }
 }
